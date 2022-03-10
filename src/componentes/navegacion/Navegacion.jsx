96e47669ce132b346d14bf4e8b740f942712d23e
Jesus Cobarrubia <jesuscobarrubia18@gmail.com>
2022-03-09 21:33:08 -0300

    'navbar'

--- src/componentes/navegacion/Navegacion.jsx
+++ src/componentes/navegacion/Navegacion.jsx
@@ -37,38 +37,3 @@ const Navegacion = () => {
 }
 
 export default Navegacion
-
-
-{/* <nav className="navbar navbar-expand-lg">
-<div className="container container-fluid">
-    <ul className="navbar-nav">
-        <li className="nav-item">
-        <motion.a className="nav-link JC" aria-current="page" href="#" 
-        animate={{
-            scale: [1, 1, 1, 1, 1],
-            rotate: [0, 0, 360, 360, 0],
-            borderRadius: ["20%", "20%", "50%", "50%", "20%"],
-            transition:{repeat: Infinity, duration: 5}
-        }}
-        >
-            JC
-        </motion.a>
-        </li>
-        <li className="nav-item px-3">
-        <Link to={'/sobreMi'} className="nav-link" aria-current="page" href="#">
-            Sobre mi
-        </Link>
-        </li>
-        <li className="nav-item px-3">
-        <Link to={'/trabajos'} className="nav-link" href="#">
-            Trabajos
-        </Link>
-        </li>
-        <li className="nav-item px-3">
-        <Link to={'/contacto'} className="nav-link" href="#">
-            Contacto
-        </Link>
-        </li>
-    </ul>
-</div>
-</nav> */}
